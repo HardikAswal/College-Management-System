5f47586b8aa7beb81a08edaffa42dd47741e5e48
HardikAswal <hardik.aswal@gmail.com>
2020-02-06 23:27:48 +0530

    Add student & faculty

--- client/src/Components/Admin/admin.js
+++ client/src/Components/Admin/admin.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import {BrowserRouter as Router,Route,Link} from "react-router-dom";
-// import admin_to_student from "./admin_to_student"
+import "./admin.css";
+// import Add_student from "./addStudent"
 
 class Admin extends React.Component{
     constructor(props){
         super(props);
-        this.state={}
+        this.state={};
+        this.state.student=[];
+        this.state.faculty=[]
+    }
+
+    addInfo(student_info){
+        console.log(student_info);
+        let student=this.state.student;
+        student.push(student_info);
+        console.log(student)
+        this.setState({
+            student:student
+        })
+        console.log(this.state.student);
+    }
+    
+    addFaculty(faculty_info){
+        console.log(faculty_info);
+        let faculty=this.state.faculty;
+        faculty.push(faculty_info);
+        console.log(faculty)
+        this.setState({
+            faculty:faculty
+        })
+        console.log(this.state.faculty);
     }
 
     render(){
@@ -14,9 +39,9 @@ class Admin extends React.Component{
                 <div id="menu">
                     <ul>
                         <Link to="/admin"><li>Admin</li></Link>
-                        <Link to="/admin/admin_student"><li>Dashboard</li></Link>
-                        <li>Student</li>
-                        <li>Faculty</li>
+                        <li>Dashboard</li>
+                        <Link to="/admin/admin_student"><li>Student</li></Link>
+                        <Link to="/admin/admin_faculty"><li>Faculty</li></Link>
                         <li>Department</li>
                         <li>Courses</li>
                         <li>Dashboard</li>
@@ -25,7 +50,9 @@ class Admin extends React.Component{
                 </div>
 
                 <div id="right">
-                    <Route path="/admin/admin_student" component={admin_to_student}></Route>
+                    <Route path="/admin/admin_faculty" render={(e)=><Admin_to_faculty addFaculty={this.addFaculty.bind(this)} faculty={this.state.faculty}></Admin_to_faculty>}></Route>
+                    <Route path="/admin/admin_student" render={(e)=><Admin_to_student addInfo={this.addInfo.bind(this)} student={this.state.student}></Admin_to_student>}></Route>    
+                    {/* <Route path="/admin/admin_student/Add_student" render={(e)=><Add_student addInfo={this.addInfo.bind(this)}></Add_student>}></Route> */}
                 </div>
             </React.Fragment>
         )
@@ -34,11 +61,85 @@ class Admin extends React.Component{
 
 export default Admin;
 
-function admin_to_student(){
+function Admin_to_student(props){
     return (
         <div>
-            Hi admin to studnent module
+            <div> 
+            <h2>Manage Student</h2>
+            <Link to="/admin/admin_student/Add_student">Add Student</Link>
+            </div>
+            <Route path="/admin/admin_student/Add_student" render={(e)=><Add_student addInfo={props.addInfo.bind(this)}></Add_student>}></Route>
+            <div>
+            {props.student.map((x)=>(
+                    <div>{x.name} {x.enroll} {x.address} {x.dob} </div>
+                    ))}
+            </div>        
         </div>
     )
+}
+
+function Admin_to_faculty(props){
+    return (
+        <div>
+            <div> 
+            <h2>Manage Faculty</h2>
+            <Link to="/admin/admin_faculty/Add_faculty">Add Faculty</Link>
+            </div>
+            <Route path="/admin/admin_faculty/Add_faculty" render={(e)=><Add_faculty addFaculty={props.addFaculty.bind(this)}></Add_faculty>}></Route>
+            <div>
+            {props.faculty.map((x)=>(
+                    <div>{x.name} {x.enroll} {x.address} {x.dob} </div>
+                    ))}
+            </div>        
+        </div>
+    )
+}
+
+function Add_student(props){
+
+    const [name, setName] = React.useState("");
+    const [enroll, setEnroll] = React.useState("");
+    const [address, setAddress] = React.useState("");
+    const [dob, setDob] = React.useState(null);
+    const [fname, setFname] = React.useState("");
+    const [mname, setMname] = React.useState("");
+
+return (
+    <div className="add_student">
+       <input type="text" name="name" placeholder="Name" onChange={(e)=>{setName(e.target.value)}} value={name}></input> 
+       <input type="number" name="enroll_no" placeholder="Enrollment Number" onChange={(e)=>{setEnroll(e.target.value)}} value={enroll}></input> 
+       <input type="text" name="address" placeholder="Address" onChange={(e)=>{setAddress(e.target.value)}} value={address}></input> 
+       <input type="date" name="dob" onChange={(e)=>{setDob(e.target.value)}} value={dob}></input> 
+       <input type="text" name="fatherName" placeholder="Father's Name" onChange={(e)=>{setFname(e.target.value)}} value={fname}></input> 
+       <input type="text" name="motherName" placeholder="Mother's Name" onChange={(e)=>{setMname(e.target.value)}} value={mname}></input>
+     
+        <button type="submit" onClick={(e)=>{props.addInfo({name,enroll,address,dob,fname,mname});setName("");setEnroll("");setAddress("");setDob(null);setFname("");setMname("")}}>Add</button>
+        <Link to="/admin/admin_student">Cancel</Link> 
+    </div>
+)
+}
+
+function Add_faculty(props){
+
+    const [name, setName] = React.useState("");
+    const [id, setId] = React.useState("");
+    const [address, setAddress] = React.useState("");
+    const [dob, setDob] = React.useState(null);
+    const [fname, setFname] = React.useState("");
+    const [mname, setMname] = React.useState("");
+
+return (
+    <div className="add_student">
+       <input type="text" name="name" placeholder="Name" onChange={(e)=>{setName(e.target.value)}} value={name}></input> 
+       <input type="number" name="id" placeholder="Enrollment Number" onChange={(e)=>{setId(e.target.value)}} value={id}></input> 
+       <input type="text" name="address" placeholder="Address" onChange={(e)=>{setAddress(e.target.value)}} value={address}></input> 
+       <input type="date" name="dob" onChange={(e)=>{setDob(e.target.value)}} value={dob}></input> 
+       <input type="text" name="fatherName" placeholder="Father's Name" onChange={(e)=>{setFname(e.target.value)}} value={fname}></input> 
+       <input type="text" name="motherName" placeholder="Mother's Name" onChange={(e)=>{setMname(e.target.value)}} value={mname}></input>
+     
+        <button type="submit" onClick={(e)=>{props.addFaculty({name,id,address,dob,fname,mname});setName("");setId("");setAddress("");setDob(null);setFname("");setMname("")}}>Add</button>
+        <Link to="/admin/admin_faculty">Cancel</Link> 
+    </div>
+)
 }
 
